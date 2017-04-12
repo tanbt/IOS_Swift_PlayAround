1f93a88f5e32ff42a427588e0fe43a1056e13fa7
TanMacVbox <tanmacvbox@tanmacvboxs-imac.local>
2017-04-12 20:20:45 +0300

    Initial Commit

---
+++ FirstIOSApp/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  FirstIOSApp
+//
+//  Created by TanMacVbox on 4/11/17.
+//  Copyright © 2017 TanMacVbox. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieImageView: UIImageView!
+    @IBOutlet var movieLabel: UILabel!
+    @IBOutlet var movieYear: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FirstIOSApp/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  FirstIOSApp
+//
+//  Created by TanMacVbox on 4/11/17.
+//  Copyright © 2017 TanMacVbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var favouriteMovies: [Movie] = []
+    @IBOutlet var mainTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favouriteMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let moviecell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath) as! CustomTableViewCell
+        
+        let idx: Int = indexPath.row
+        moviecell.movieLabel?.text = favouriteMovies[idx].title
+        moviecell.movieYear?.text = favouriteMovies[idx].year
+        displayMovieImage(row: idx, moviecell: moviecell)
+        
+        return moviecell
+    }
+    
+    func displayMovieImage (row: Int, moviecell: CustomTableViewCell) {
+        let url: String = (URL(string: favouriteMovies[row].imageUrl)?.absoluteString)!
+        URLSession.shared.dataTask(with: URL(string:url)!, completionHandler: {(data, response, error) -> Void in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                let image = UIImage(data: data!)
+                moviecell.movieImageView?.image = image
+            })
+        }).resume()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        mainTableView.reloadData();
+        if favouriteMovies.count == 0 {
+            favouriteMovies.append(Movie(id: "ID123456", title: "This is the title", year: "2017", imageUrl: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE1ODgwOTkzNF5BMl5BanBnXkFtZTgwMDcwMTg5MTI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"))
+        }
+        
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
